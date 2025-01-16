d8a093bd6229ca9af8a871ee4ef41a699ac9131f
Sara <moniquesarah5@gmail.com>
2025-01-16 13:56:33 -0300

    teste 1

---
+++ main.js
@@ -0,0 +1,12 @@
+let nome = document.getElementById('inome')
+let tel = document.getElementById('iphone')
+
+
+
+
+function cadastrar(){
+
+    if(nome.value == 0 && tel.value == 0){
+        window.alert('Por favor, insira os dados primeiro')
+    }
+}
